0d5185f36e1177098d9212fbb64b1055c7996f7a
bickrom-shorma <bickrombickrom21@gmail.com>
2026-07-15 19:57:29 +0600

    decleare a generator password function

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -11,3 +11,12 @@ st.page(
 st.title("🔐 Password Generator")
 
 len = st.slider("select Password Length" )
+
+uppercase = st.checkbox("Uppercase Letters",value=True)
+lowercase = st.checkbox("Lower Letters",value=True)
+digit = st.checkbox("Number Letters",value=True)
+Symbols = st.checkbox("Symbols Letters",value=True)
+
+def Generate_password(len):
+    ch = ""
+    
