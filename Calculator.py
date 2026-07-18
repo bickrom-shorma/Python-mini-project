5120bae56efcdf3fdbc68b91b120223d6e475e36
bickrom-shorma <bickrombickrom21@gmail.com>
2026-07-18 20:29:41 +0600

    Add calculator basic structure

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+print("=====Simple Calculator=====")
+
+a1 = int(input("Enter a number :"))
+a2 = int(input("Enter a number :"))
+
+# Feature 
+print("Choose an Operation :")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multipication (*)")
+print("4. Division (/)")
+print("5. Modulus (%)")
+print("6. Power (**)")
+print("7. Floor Division (//)")
+
+
